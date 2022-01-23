fcc1ad29ef1b39253608310ac0d449ab17ee2a8b
Yuri Mayk <yuri.mayk@gmail.com>
2022-01-23 19:48:58 -0300

    Updated README.md and complemented MapChallenge.js.

---
+++ MapChallenge.js
@@ -0,0 +1,15 @@
+//Using Map concept, transform the list below adding a third property in each object. Name that property "sector" that will acept two values ('Black' and 'Green'). All the VIP will receive the BlackPassport. And All the Non-VIP will receive the GreenPassport.
+
+const list = [
+    {name:"Jonas", isVip:true},
+    {name:"Heleonor", isVip:false},
+    {name:"Caleb", isVip:true},
+    {name:"Greg", isVip:true},
+    {name:"Chris", isVip:false},
+    {name:"Nuna", isVip:false},
+    {name:"Helen", isVip:false},
+    {name:"Júlia", isVip:true},
+    {name:"Hector", isVip:true},
+    {name:"Elen", isVip:true},
+]
+
