0fd5bd49d79b5d5e6b8f49d9b672d129b570852f
Yuri Mayk <yuri.mayk@gmail.com>
2022-01-23 20:08:16 -0300

    Solved the MapChallenge.js.

--- MapChallenge.js
+++ MapChallenge.js
@@ -1,15 +1,20 @@
 //Using Map concept, transform the list below adding a third property in each object. Name that property "sector" that will acept two values ('Black' and 'Green'). All the VIP will receive the BlackPassport. And All the Non-VIP will receive the GreenPassport.
 
 const list = [
-    {name:"Jonas", isVip:true},
-    {name:"Heleonor", isVip:false},
-    {name:"Caleb", isVip:true},
-    {name:"Greg", isVip:true},
-    {name:"Chris", isVip:false},
-    {name:"Nuna", isVip:false},
-    {name:"Helen", isVip:false},
-    {name:"Júlia", isVip:true},
-    {name:"Hector", isVip:true},
-    {name:"Elen", isVip:true},
+    { name: "Jonas", isVip: true },
+    { name: "Heleonor", isVip: false },
+    { name: "Caleb", isVip: true },
+    { name: "Greg", isVip: true },
+    { name: "Chris", isVip: false },
+    { name: "Nuna", isVip: false },
+    { name: "Helen", isVip: false },
+    { name: "Júlia", isVip: true },
+    { name: "Hector", isVip: true },
+    { name: "Elen", isVip: true },
 ]
 
+const newList = list.map((person) => {
+    person.isVip === true ? person = {name:person.name, isvip:person.isVip, sector: "Black"} : person = {name:person.name, isvip:person.isVip, sector: "Green"}
+    return person
+})
+console.log(newList)
